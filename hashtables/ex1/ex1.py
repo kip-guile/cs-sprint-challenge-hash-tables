1cf071be5c8e800a46346e3ec0fbb9a86cb99aae
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-07-10 08:46:27 +0100

    ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,18 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    output = []
+    for i in range(length):
+        if (weights[i]) in cache:
+            otherWeight = cache[weights[i]]
+            output.append(i)
+            output.append(weights.index(otherWeight))
+            return output
+        else:
+            cache[limit - weights[i]] = weights[i]
 
     return None
+
+
+print(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21))
